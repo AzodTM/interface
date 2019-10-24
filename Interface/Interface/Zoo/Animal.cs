04108289b9495d92f9c9dd47da5fd3ca3335c24b
Azod <Azod@AZODPC>
2019-10-24 23:19:04 +0500

    Create Base Class & Interface

---
+++ Interface/Interface/Zoo/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface.Zoo
+{
+    class Animal
+    {
+        int size;
+        int[] coordinate;
+        string voise;
+    }
+}
